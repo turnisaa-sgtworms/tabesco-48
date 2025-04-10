8156f53c22d06126521dc26af366ddab8f751865
newx <156273295+turnisaa-sgtworms@users.noreply.github.com>
2025-04-09 23:57:02 -0600

    added case files

---
+++ firmware/tabesco_48-vial/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x4C, 0x6E, 0xCC, 0xD5, 0x01, 0x98, 0x9E, 0x8F}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 9 }
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
